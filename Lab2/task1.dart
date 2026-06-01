2362fbf7516045d4386b0ecb2ff5f36713dc8e30
Trần Phương Huyền <tranphuonghuyen2005@gmail.com>
2026-06-01 11:35:08 +0700

    feat: making outline and  doing task 1 lab 2

---
+++ Lab2/task1.dart
@@ -0,0 +1,20 @@
+// Task 1: Declare variables of different data types and print their values.
+void main() {
+  int a = 5;
+  double b = 3.14;
+  String name = "Alice";
+  bool isStudent = true;
+  print("Integer: $a");
+  print("Double: $b");
+  print("String: $name");
+  print("Boolean: $isStudent");
+
+  var c = 10;
+  var d = 3.14;
+  var name2 = "Bob";
+  var isTeacher = false;
+  print("$c, ${c.runtimeType}");
+  print("$d, ${d.runtimeType}");
+  print("$name2, ${name2.runtimeType}");
+  print("$isTeacher, ${isTeacher.runtimeType}");
+}
